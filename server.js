632021365673ec54a22ffdde908ed88258574041
Christian P. Sarason <cpsarason@immersedin.science>
2018-07-20 13:32:42 -0700

    updates for seventy48 pages

--- server.js
+++ server.js
@@ -4,13 +4,29 @@ var path = require('path');
 // Create an server
 var server = express();
 
+// server.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   express.static(path.resolve(__dirname, 'www'));
+//   next();
+// });
+
+server.get('/', function(req, res, next) {
+  res.sendFile(`${__dirname}/www/404.html`);
+  // Handle the get for this route
+});
+
+server.post('/', function(req, res, next) {
+ // Handle the post for this route
+});
+
 // Serve the www directory
 server.use(express.static(path.resolve(__dirname, 'www')));
 
 // Send 404 for unknown routes
-server.get('*', (request, response) => {
-  response.sendFile(`${__dirname}/www/404.html`);
-});
+// server.get('*', (request, response) => {
+//   response.sendFile(`${__dirname}/www/404.html`);
+// });
 
 // Start the server on port 3000
 server.listen(3000, () => {
